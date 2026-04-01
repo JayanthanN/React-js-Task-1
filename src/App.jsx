8f809abd512c098b77cd0e43288f08e4cdb03aef
Jayanth <jayanthann@thestackly.com>
2026-04-01 16:05:04 +0530

    Task 1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+
+function App() {
+  return (
+    < >
+      
+      {/* <h1>My Introduction</h1>
+
+      <h2>Name: Jayanthan</h2>
+      <p>Email: jayanthan@email.com</p>
+      <p>Role: Frontend Developer (Learner)</p>
+
+      <p>
+        Hello! I'm Jayanthanan, a passionate learner exploring React JS 
+        and building modern web applications.
+      </p> */}
+
+
+    
+    </>
+  );
+}
+
+export default App;
